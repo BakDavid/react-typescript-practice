c3085c66cd19b0f0f4cd5b2ce6915c80a052797f
Bak David <bak.david96@gmail.com>
2022-05-18 14:14:01 +0300

    Creating todo model and using it for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("item1"), new Todo("item2")];
+
   return (
     <div>
-      <Todos items={["item1", "item2"]} />
+      <Todos items={todos} />
     </div>
   );
 }
